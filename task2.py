e9338277186229591bee1121cabb51b7b9384293
aidan138 <126530615+aidan138@users.noreply.github.com>
2025-01-17 15:24:57 -0800

    Verified verify functionality

--- task2.py
+++ task2.py
@@ -1,8 +1,9 @@
-from ebc import ecb_encryption
+from cbc import encrypt_cbc
 from Crypto.Random import get_random_bytes
 from Crypto.Cipher import AES
 
 key = get_random_bytes(AES.block_size)
+iv = get_random_bytes(16)  
 
 def submit(userdata: str):
     url_list = []
@@ -15,16 +16,21 @@ def submit(userdata: str):
             url_list.append(c)
     url_encoded = ''.join(url_list)
     raw_text = "userid=456;userdata=" + url_encoded + ";session-id=31337"
-    encryption = ecb_encryption(raw_text.encode(), key) # TODO replace with cbc_encryption instead
+    encryption = encrypt_cbc(raw_text.encode(), key, iv) # TODO replace with cbc_encryption instead
     return encryption
 
 def verify(data):
-    cipher = AES.new(key, mode=AES.MODE_CBC)
+    cipher = AES.new(key, mode=AES.MODE_CBC, iv=iv)
     decryption_text = cipher.decrypt(data)
-    print(decryption_text)
     hr_text = decryption_text.decode()
+    print(hr_text)
     if ';admin=true;' in  hr_text:
         return True
     else:
         return False
-    
+
+if __name__ == '__main__':
+    s_str = 'admin/True'
+    encryption = submit(s_str)
+    print(encryption)
+    print(verify(encryption))
