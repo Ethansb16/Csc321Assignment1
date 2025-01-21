b9ee6f3363b1bfe90b733a1f57b7f417f4d0912c
ethanbernhardt <esbernha@calpoly.edu>
2025-01-21 15:02:54 -0800

    closer on lab 2

--- task2.py
+++ task2.py
@@ -16,21 +16,40 @@ def submit(userdata: str):
             url_list.append(c)
     url_encoded = ''.join(url_list)
     raw_text = "userid=456;userdata=" + url_encoded + ";session-id=31337"
-    encryption = encrypt_cbc(raw_text.encode(), key, iv) # TODO replace with cbc_encryption instead
+    print("Original text: \n" + raw_text)
+    encryption = encrypt_cbc(raw_text.encode(), key, iv)
     return encryption
 
 def verify(data):
+    
     cipher = AES.new(key, mode=AES.MODE_CBC, iv=iv)
-    decryption_text = cipher.decrypt(data)
-    hr_text = decryption_text.decode()
-    print(hr_text)
-    if ';admin=true;' in  hr_text:
-        return True
-    else:
+    decrypted_text = cipher.decrypt(data)
+    try:
+        hr_text = decrypted_text.decode() 
+        print("Decrypted Text:", hr_text)
+        return ';admin=true;' in hr_text
+    except UnicodeDecodeError:
+        print("Decryption failed")
         return False
 
+def flip_bit(ciphertext, position, bit_mask):
+    ciphertext_mutable = bytearray(ciphertext)
+    ciphertext_mutable[position] ^= bit_mask
+    return bytes(ciphertext_mutable)
+
+
 if __name__ == '__main__':
-    s_str = 'admin/True'
-    encryption = submit(s_str)
-    print(encryption)
-    print(verify(encryption))
+    try:         
+        s_str = 'admin/True'
+        encryption = submit(s_str)
+        print(encryption)
+        position_to_flip = AES.block_size + 11
+        modified_encrypted_data = flip_bit(encryption, position_to_flip, 0x10)  # / -> =
+        print(verify(encryption))
+        #print("Original message:", msg)
+        #print("Message length:", len(msg))
+    
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    
